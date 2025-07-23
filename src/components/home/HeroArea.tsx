e718ae9c9915ebd960e92cc965e28a6f5ecfd2ac
Syed Munaf Ahmed <syedmunafahmed02@gmail.com>
2025-07-23 16:07:33 +0500

    Updated homepage content

--- src/components/home/HeroArea.tsx
+++ src/components/home/HeroArea.tsx
@@ -8,7 +8,7 @@ export default function HeroArea() {
             <div className="col-lg-12">
 
               <div className="hero-content wow fadeInUp text-center delay-0-2s">
-                <h3>Syed Munaf Ahmed</h3>
+                <h4>Syed Munaf Ahmed</h4>
               </div>
 
             </div>
